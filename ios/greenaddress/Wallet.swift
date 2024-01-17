8ce5b28a79f23849332f9d7cf289e4cb546e6386
joaolobo <joao.lobom@gmail.com>
2024-01-17 19:57:40 +0000

    basic wallet init

--- ios/greenaddress/Wallet.swift
+++ ios/greenaddress/Wallet.swift
@@ -1,6 +1,65 @@
 import Flutter
 
-//use this just to implement sission init (with connect-
+public class GDKWallet {
+    var SUBACCOUNT_TYPE = "2of2"
+    var SUBACCOUNT_NAME = ""
+    var PIN_DATA_FILENAME = "pin_data.json"
+    var mnemonic: String?
+    var session: GDKSession?
+    var subaccountPointer: Any?
+    var lastBlockHeight = 0
+
+    // Class method to create and return an instance of GDKWallet
+    class func createNewWallet(createWith2FAEnabled: Bool, mnemonic: String? = nil) throws -> GDKWallet {
+        let wallet = GDKWallet()
+
+        // You can pass in a mnemonic generated outside GDK if you want, or have
+        // GDK generate it for you by omitting it. 2FA is enabled if chosen and
+        // can be enabled/disabled at any point.
+        wallet.mnemonic = mnemonic ?? ""
+
+        // Session network name options are: testnet, mainnet.
+        wallet.session = try? GDKSession()
+        try? wallet.session?.connect(netParams: ["name": "testnet"])
+
+        let credentials = ["mnemonic": wallet.mnemonic]
+        try? wallet.session?.registerUserSW(details: credentials).call()
+        try? wallet.session?.loginUserSW(details: credentials).call()
+        try? wallet.fetchBlockHeight()
+
+        if createWith2FAEnabled {
+            try? wallet.twofactorAuthEnabled(true)
+        }
+
+        return wallet
+    }
+
+    // Method to fetch block height
+    func fetchBlockHeight() throws {
+        // New blocks are added to notifications as they are found, so we need to
+        // find the latest or, if there hasn't been one since we last checked,
+        // use the value set during login in the session's login method.
+        // The following provides an example of using GDK's notification queue.
+
+//        let q = self.session?.notifications
+//        while let notification = try? q?.get(block: true, timeout: 1) {
+//            if let event = notification["event"] as? String, event == "block" {
+//                if let blockHeight = notification["block"]?["block_height"] as? Int,
+//                    blockHeight > self.lastBlockHeight {
+//                    self.lastBlockHeight = blockHeight
+//                }
+//            }
+//        }
+    }
+
+    // Method to enable/disable two-factor authentication
+    func twofactorAuthEnabled(_ isEnabled: Bool) throws {
+        // Implementation for enabling/disabling two-factor authentication
+    }
+}
+
+
+
 public class GDKSession: Session {
     public var ephemeral = false
     public var netParams = [String: Any]()
@@ -39,6 +98,38 @@ public class GDKSession: Session {
     }
 }
 
+public struct GdkInit: Codable {
+    enum CodingKeys: String, CodingKey {
+        case datadir
+        case tordir
+        case registrydir
+        case logLevel = "log_level"
+    }
+    public let datadir: String?
+    public let tordir: String?
+    public let registrydir: String?
+    public let logLevel: String
+    public var breezSdkDir: String { "\(datadir ?? "")/breezSdk" }
+    
+    public static func defaults() -> GdkInit {
+        let appSupportDir = try? FileManager.default.url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        let cacheDir = try? FileManager.default.url(for: .cachesDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        var logLevel = "none"
+#if DEBUG
+        logLevel = "info"
+#endif
+        return GdkInit(datadir: appSupportDir?.path,
+                       tordir: cacheDir?.path,
+                       registrydir: cacheDir?.path,
+                       logLevel: logLevel)
+    }
+    
+    public func run() {
+        try? gdkInit(config: self.toDict() ?? [:])
+        
+    }
+}
+
 public class Wallet {
     private var channel: FlutterMethodChannel?
 
@@ -57,13 +148,15 @@ public class Wallet {
     }
 
     private func getMnemonic(result: @escaping FlutterResult) {
+        let walletInit = GdkInit.defaults().run()
+        
         do {
-            let mnemonic = try greenaddress.generateMnemonic12()
-            result(mnemonic)
+            let newWallet = try GDKWallet.createNewWallet(createWith2FAEnabled: true)
+            // Handle successful creation, possibly by sending wallet data back to Flutter
+            result(newWallet)
         } catch {
-            result(FlutterError(code: "UNAVAILABLE",
-                                message: "Mnemonic generation failed",
-                                details: nil))
+            // Handle error, possibly by sending an error message back to Flutter
+            result("WALLET_CREATION_ERROR: Failed to create wallet")
         }
     }
 }
---
+++ ios/greenaddress/Wally.swift
@@ -0,0 +1,223 @@
+import Foundation
+import ga.wally
+
+public class Wally {
+
+    public static let WALLY_EC_SIGNATURE_RECOVERABLE_LEN = EC_SIGNATURE_RECOVERABLE_LEN
+    public static let WALLY_BLINDING_FACTOR_LEN = BLINDING_FACTOR_LEN
+    public static let BIP39_WORD_LIST_LANG = "en"
+    public static let AES_BLOCK_LEN = 16
+    public static let HMAC_SHA256_LEN = 32
+    public static let EC_PRIVATE_KEY_LEN = 32
+
+    public static func sigToDer(sig: [UInt8]) throws -> [UInt8] {
+        let sigPtr = UnsafePointer(sig)
+        let derPtr = UnsafeMutablePointer<UInt8>.allocate(capacity: Int(EC_SIGNATURE_DER_MAX_LEN))
+        var written: Int = 0
+        if wally_ec_sig_to_der(sigPtr, sig.count, derPtr, Int(EC_SIGNATURE_DER_MAX_LEN), &written) != WALLY_OK {
+            throw GaError.GenericError()
+        }
+        let der = Array(UnsafeBufferPointer(start: derPtr, count: written))
+        // derPtr.deallocate()
+        return der
+    }
+
+    public static func bip32KeyFromParentToBase58(isMainnet: Bool = true, pubKey: [UInt8], chainCode: [UInt8], branch: UInt32 ) throws -> String {
+        let version = isMainnet ? BIP32_VER_MAIN_PUBLIC : BIP32_VER_TEST_PUBLIC
+        var subactkey: UnsafeMutablePointer<ext_key>?
+        var branchkey: UnsafeMutablePointer<ext_key>?
+        var xpubPtr: UnsafeMutablePointer<Int8>?
+        let pubKey_: UnsafePointer<UInt8> = UnsafePointer(pubKey)
+        let chainCode_: UnsafePointer<UInt8> = UnsafePointer(chainCode)
+        defer {
+            bip32_key_free(subactkey)
+            bip32_key_free(branchkey)
+            wally_free_string(xpubPtr)
+        }
+        if bip32_key_init_alloc(UInt32(version), UInt32(0), UInt32(0), chainCode_, chainCode.count,
+                                pubKey_, pubKey.count, nil, 0, nil, 0, nil, 0, &subactkey) != WALLY_OK {
+            throw GaError.GenericError()
+        }
+        if bip32_key_from_parent_alloc(subactkey, branch, UInt32(BIP32_FLAG_KEY_PUBLIC | BIP32_FLAG_SKIP_HASH), &branchkey) != WALLY_OK {
+            throw GaError.GenericError()
+        }
+        if bip32_key_to_base58(branchkey, UInt32(BIP32_FLAG_KEY_PUBLIC), &xpubPtr) != WALLY_OK {
+            throw GaError.GenericError()
+        }
+        return String(cString: xpubPtr!)
+    }
+
+    public static func sha256d(_ input: [UInt8]) throws -> [UInt8] {
+        let inputPtr: UnsafePointer<UInt8> = UnsafePointer(input)
+        let outputPtr = UnsafeMutablePointer<UInt8>.allocate(capacity: Int(SHA256_LEN))
+        if wally_sha256d(inputPtr, input.count, outputPtr, Int(SHA256_LEN)) != WALLY_OK {
+            throw GaError.GenericError()
+        }
+        return Array(UnsafeBufferPointer(start: outputPtr, count: Int(SHA256_LEN)))
+    }
+
+    public static func asset_final_vbf(values: [UInt64], numInputs: Int, abf: [UInt8], vbf: [UInt8]) throws -> [UInt8] {
+        let valuesPtr: UnsafePointer<UInt64> = UnsafePointer(values)
+        let abfPtr: UnsafePointer<UInt8> = UnsafePointer(abf)
+        let vbfPtr: UnsafePointer<UInt8> = UnsafePointer(vbf)
+        let len = Int(BLINDING_FACTOR_LEN)
+        let bufferPtr = UnsafeMutablePointer<UInt8>.allocate(capacity: len)
+        if wally_asset_final_vbf(valuesPtr, values.count, numInputs, abfPtr, abf.count, vbfPtr, vbf.count, bufferPtr, len) != WALLY_OK {
+            throw GaError.GenericError()
+        }
+        return Array(UnsafeBufferPointer(start: bufferPtr, count: len))
+    }
+
+    public static func flatten(_ inputs: [[UInt8]], fixedSize: Int32?) -> [UInt8] {
+        return inputs
+            .reduce([UInt8](), { (prev, item) in
+                if let size = fixedSize, item.count < size {
+                    return prev + item + [UInt8](repeating: 0, count: Int(size) - item.count)
+                }
+                return prev + item
+            })
+    }
+
+    public static func bip32KeyFromBase58(_ input: String) throws -> ga.ext_key {
+        var output: UnsafeMutablePointer<ga.ext_key>?
+        let base58: UnsafeMutablePointer<CChar> = strdup(input)!
+        if bip32_key_from_base58_alloc(base58, &output) != WALLY_OK {
+            throw GaError.GenericError()
+        }
+        guard let output = output else {
+            throw GaError.GenericError()
+        }
+        return output.pointee
+    }
+
+    public static func bip85FromMnemonic(
+        mnemonic: String,
+        passphrase: String?,
+        isTestnet: Bool = false,
+        index: UInt32 = 0,
+        numOfWords: UInt32 = 12
+    ) -> String? {
+        let version = isTestnet ? BIP32_VER_TEST_PRIVATE : BIP32_VER_MAIN_PRIVATE
+        let seed512Ptr = UnsafeMutablePointer<UInt8>.allocate(capacity: Int(BIP39_SEED_LEN_512))
+        var bip32KeyPtr: UnsafeMutablePointer<ga.ext_key>?
+        let bip85Ptr = UnsafeMutablePointer<UInt8>.allocate(capacity: Int(HMAC_SHA512_LEN))
+        var bip85Len: Int = 0
+        var wordlistPtr: OpaquePointer?
+        var resultPtr: UnsafeMutablePointer<CChar>?
+        defer {
+            bip32_key_free(bip32KeyPtr)
+        }
+        if bip39_mnemonic_to_seed512(strdup(mnemonic)!, passphrase != nil ? strdup(passphrase ?? "")! : nil , seed512Ptr, Int(BIP39_SEED_LEN_512)) != WALLY_OK {
+            return nil
+        }
+        if bip32_key_from_seed_alloc(seed512Ptr, Int(BIP39_SEED_LEN_512), UInt32(version), UInt32(BIP32_FLAG_SKIP_HASH), &bip32KeyPtr) != WALLY_OK {
+            return nil
+        }
+        if bip85_get_bip39_entropy(bip32KeyPtr, strdup(Wally.BIP39_WORD_LIST_LANG)!, numOfWords, index, bip85Ptr, Int(HMAC_SHA512_LEN), &bip85Len) != WALLY_OK {
+            return nil
+        }
+        if bip39_get_wordlist(strdup(Wally.BIP39_WORD_LIST_LANG)!, &wordlistPtr) != WALLY_OK {
+            return nil
+        }
+        if bip39_mnemonic_from_bytes(wordlistPtr, bip85Ptr, bip85Len, &resultPtr) != WALLY_OK {
+            return nil
+        }
+        guard let resultPtr = resultPtr else {
+            return nil
+        }
+        return String(cString: resultPtr)
+    }
+
+    public static func getHashPrevouts(
+        txhashes: [UInt8],
+        outputIdxs: [UInt32]
+    ) -> [UInt8]? {
+        let txhashesPtr: UnsafePointer<UInt8> = UnsafePointer(txhashes)
+        let outputIdxsPtr: UnsafePointer<UInt32> = UnsafePointer(outputIdxs)
+        let len = Int(SHA256_LEN)
+        let bufferPtr = UnsafeMutablePointer<UInt8>.allocate(capacity: len)
+        if wally_get_hash_prevouts(txhashesPtr, txhashes.count, outputIdxsPtr, outputIdxs.count, bufferPtr, len) != WALLY_OK {
+            return nil
+        }
+        return Array(UnsafeBufferPointer(start: bufferPtr, count: len))
+    }
+
+    public static func txFromBytes(
+        tx: [UInt8],
+        elements: Bool = false,
+        segwit: Bool = false
+    ) -> UnsafeMutablePointer<ga.wally_tx>? {
+        let txPtr: UnsafePointer<UInt8> = UnsafePointer(tx)
+        var bufferPtr: UnsafeMutablePointer<ga.wally_tx>?
+        let flag = elements ? WALLY_TX_FLAG_USE_ELEMENTS : segwit ? WALLY_TX_FLAG_USE_WITNESS : 0
+        if wally_tx_from_bytes(txPtr, tx.count, UInt32(flag), &bufferPtr) != WALLY_OK {
+            return nil
+        }
+        return bufferPtr
+    }
+
+    public static func txVersion(
+        wallyTx: UnsafeMutablePointer<ga.wally_tx>
+    ) -> UInt32 {
+        return wallyTx.pointee.version
+    }
+
+    public static func txLocktime(
+        wallyTx: UnsafeMutablePointer<ga.wally_tx>
+    ) -> UInt32 {
+        return wallyTx.pointee.locktime
+    }
+
+    public static func txGetOutputAsset(
+        wallyTx: UnsafeMutablePointer<ga.wally_tx>,
+        index: Int
+    ) -> [UInt8]? {
+        let output = wallyTx.pointee.outputs[index]
+        return Array(UnsafeBufferPointer(start: output.asset, count: output.asset_len))
+    }
+
+    public static func txGetOutputValue(
+        wallyTx: UnsafeMutablePointer<ga.wally_tx>,
+        index: Int
+    ) -> [UInt8]? {
+        let output = wallyTx.pointee.outputs[index]
+        return Array(UnsafeBufferPointer(start: output.value, count: output.value_len))
+    }
+    
+    public static func ecPrivateKeyVerify(
+        privateKey: [UInt8]
+    ) -> Bool {
+        let keyPtr: UnsafePointer<UInt8> = UnsafePointer(privateKey)
+        return wally_ec_private_key_verify(keyPtr, privateKey.count) == WALLY_OK
+    }
+    public static func bip85FromJade(
+        privateKey: [UInt8],
+        publicKey: [UInt8],
+        label: String,
+        payload: [UInt8]
+    ) -> String? {
+        let privPtr: UnsafePointer<UInt8> = UnsafePointer(privateKey)
+        let pubPtr: UnsafePointer<UInt8> = UnsafePointer(publicKey)
+        let payloadPtr: UnsafePointer<UInt8> = UnsafePointer(payload)
+        let labelPtr = strdup(label)!
+        let bip85Ptr = UnsafeMutablePointer<UInt8>.allocate(capacity: payload.count)
+        var bip85Len: Int = 0
+        var wordlistPtr: OpaquePointer?
+        var resultPtr: UnsafeMutablePointer<CChar>?
+            
+        if wally_aes_cbc_with_ecdh_key(privPtr, privateKey.count, nil, 0, payloadPtr, payload.count, pubPtr, publicKey.count, labelPtr, label.count, UInt32(AES_FLAG_DECRYPT), bip85Ptr, payload.count, &bip85Len) != WALLY_OK {
+            return nil
+        }
+        if bip39_get_wordlist(strdup(Wally.BIP39_WORD_LIST_LANG)!, &wordlistPtr) != WALLY_OK {
+            return nil
+        }
+        if bip39_mnemonic_from_bytes(nil, bip85Ptr, bip85Len, &resultPtr) != WALLY_OK {
+            return nil
+        }
+        guard let resultPtr = resultPtr else {
+            return nil
+        }
+        return String(cString: resultPtr)
+    }
+}
+
