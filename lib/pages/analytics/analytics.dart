e27a12210f16b303a2fd8d4935ded538336df7f2
joaolobo <joao.lobom@gmail.com>
2024-03-02 15:12:20 +0000

    add necessary actions for swaps

--- lib/pages/analytics/analytics.dart
+++ lib/pages/analytics/analytics.dart
@@ -8,9 +8,15 @@ class Analytics extends StatelessWidget {
       child: Text('This is the Account Component'),
     );
   }
+}
 //   all the transactions done by the user
 //   all the conversions done
 //   a fl chart with money spend on fees and money sent
 //   a table with ongoing transactions
 
-}
+
+// ------
+
+// A simple chart with all fees and money sent
+// transactinos below
+// a chart displaing balancing costs
