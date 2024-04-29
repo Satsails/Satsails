6d1794b797bdcda3240776eac3076541e06a4d66
joaolobo <joao.lobom@gmail.com>
2024-04-29 13:26:21 +0100

    fix btc expanses

--- lib/screens/analytics/components/expenses_diagram.dart
+++ lib/screens/analytics/components/expenses_diagram.dart
@@ -13,92 +13,55 @@ class ExpensesDiagram extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final bitcoinTransactions = ref.watch(bitcoinTransactionsByDate);
     final btcFormat = ref.watch(settingsProvider).btcFormat;
-    final dynamicWidth = MediaQuery.of(context).size.width;
-    final dynamicHeight = MediaQuery.of(context).size.height;
 
     return Column(
       children: [
-        SizedBox(
-          width: dynamicWidth,
-          height: dynamicHeight / 5,
-          child: _calculateBitcoinExpenses(bitcoinTransactions).total == 0
-              ? PieChart(PieChartData(sections: [PieChartSectionData(value: 1, title: '', radius: 20, color: Colors.grey)], borderData: FlBorderData(show: false)))
-              : PieChart(PieChartData(
-            sections: [
-              PieChartSectionData(value: _calculateBitcoinExpenses(bitcoinTransactions).sent.toDouble(), title: '', radius: 20, badgeWidget: const Icon(Icons.arrow_downward, color: Colors.white), color: Colors.greenAccent),
-              PieChartSectionData(value: _calculateBitcoinExpenses(bitcoinTransactions).received.toDouble(), title: '', radius: 20, badgeWidget: const Icon(Icons.arrow_upward, color: Colors.white), color: Colors.redAccent),
-              PieChartSectionData(value: _calculateBitcoinExpenses(bitcoinTransactions).fee.toDouble(), title: '', radius: 20, badgeWidget: const Icon(MingCute.pickax_fill, color: Colors.white), color: Colors.deepOrange),
-            ],
-            borderData: FlBorderData(show: false),
-          )),
-        ),
-        SizedBox(height: dynamicHeight / 20),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Card(
-              elevation: 4,
-              child: Container(
-                decoration: BoxDecoration(
-                  gradient: const LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: [Colors.red, Colors.redAccent],
-                  ),
-                  borderRadius: BorderRadius.circular(6.0),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'Sent: ${_calculateBitcoinExpenses(bitcoinTransactions).convertToDenomination(btcFormat).sent}',
-                    style: const TextStyle(color: Colors.white, fontSize: 13),
-                  ),
-                ),
-              ),
-            ),
-            Card(
-              elevation: 4,
-              child: Container(
-                decoration: BoxDecoration(
-                  gradient: const LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: [Colors.greenAccent, Colors.greenAccent],
-                  ),
-                  borderRadius: BorderRadius.circular(6.0), // Add this line
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'Received: ${_calculateBitcoinExpenses(bitcoinTransactions).convertToDenomination(btcFormat).received}',
-                    style: const TextStyle(color: Colors.white, fontSize: 13),
-                  ),
-                ),
-              ),
-            ),
-            Card(
-              elevation: 4,
-              child: Container(
-                decoration: BoxDecoration(
-                  gradient: const LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: [Colors.deepOrange, Colors.orangeAccent],
-                  ),
-                  borderRadius: BorderRadius.circular(6.0),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'Fees: ${_calculateBitcoinExpenses(bitcoinTransactions).convertToDenomination(btcFormat).fee}',
-                    style: const TextStyle(color: Colors.white, fontSize: 13),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        )
+            _buildCard('Sent',_calculateBitcoinExpenses(bitcoinTransactions).convertToDenomination(btcFormat).sent, [Colors.red, Colors.redAccent], context),
+            _buildCard('Received',_calculateBitcoinExpenses(bitcoinTransactions).convertToDenomination(btcFormat).received, [Colors.green, Colors.greenAccent], context),
+            _buildCard('Fee',_calculateBitcoinExpenses(bitcoinTransactions).convertToDenomination(btcFormat).fee, [Colors.deepOrange, Colors.orangeAccent], context),
+          ],),
       ],
+    );
+  }
+
+  Widget _buildCard(String title, double value, List<Color> gradientColors, BuildContext context) {
+    final dynamicHeight = MediaQuery.of(context).size.height;
+    final dynamicWidth = MediaQuery.of(context).size.width;
+    return Container(
+      width: dynamicWidth / 3.5,
+      height: dynamicHeight / 7,
+      child: Card(
+        elevation: 10,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: gradientColors,
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                title,
+                style: TextStyle(color: Colors.white, fontSize: 14, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                value.toString(),
+                style: TextStyle(color: Colors.white, fontSize: 14),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 
