0fb98e24f459804b92f537efe8150973fbb68da3
joaolobo <joao.lobom@gmail.com>
2024-08-24 22:38:32 +0100

    some refacring

--- lib/screens/exchange/components/liquid_swap_cards.dart
+++ lib/screens/exchange/components/liquid_swap_cards.dart
@@ -16,6 +16,7 @@ import 'package:Satsails/providers/balance_provider.dart';
 import 'package:Satsails/providers/send_tx_provider.dart';
 import 'package:Satsails/providers/settings_provider.dart';
 import 'package:Satsails/providers/sideswap_provider.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 final currentBalanceProvider = StateProvider.autoDispose<String>((ref) {
   final btcFormat = ref.read(settingsProvider).btcFormat;
@@ -71,162 +72,22 @@ class _LiquidSwapCardsState extends ConsumerState<LiquidSwapCards> {
         children: [
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: sendBitcoin
+            children: ref.read(sendBitcoinProvider)
                 ? [
               buildCard('Liquid', 'BTC', Colors.blueAccent, Colors.deepPurple, ref, context, true, AssetId.LBTC, titleFontSize),
               SizedBox(height: dynamicSizedBox),
-              SizedBox(
-                height: dynamicCardHeight,
-                child: CardSwiper(
-                  scale: 0,
-                  padding: const EdgeInsets.all(0),
-                  allowedSwipeDirection: const AllowedSwipeDirection.symmetric(vertical: true),
-                  cardsCount: cards.length,
-                  onSwipe: (int previousIndex, int? currentIndex, CardSwiperDirection direction) {
-                    AssetId ticker;
-                    switch (currentIndex) {
-                      case 0:
-                        ticker = AssetId.BRL;
-                        break;
-                      case 1:
-                        ticker = AssetId.USD;
-                        break;
-                      case 2:
-                        ticker = AssetId.EUR;
-                        break;
-                      default:
-                        ticker = AssetId.BRL;
-                        break;
-                    }
-                    setState(() {
-                      ref.read(tickerProvider.notifier).state = ticker;
-                    });
-
-                    if (!sendBitcoin) {
-                      switch (ticker) {
-                        case AssetId.BRL:
-                          ref.read(currentBalanceProvider.notifier).state = fiatInDenominationFormatted(ref.watch(balanceNotifierProvider).brlBalance);
-                          break;
-                        case AssetId.USD:
-                          ref.read(currentBalanceProvider.notifier).state = fiatInDenominationFormatted(ref.watch(balanceNotifierProvider).usdBalance);
-                          break;
-                        case AssetId.EUR:
-                          ref.read(currentBalanceProvider.notifier).state = fiatInDenominationFormatted(ref.watch(balanceNotifierProvider).eurBalance);
-                          break;
-                        default:
-                          ref.read(currentBalanceProvider.notifier).state = fiatInDenominationFormatted(ref.watch(balanceNotifierProvider).brlBalance);
-                          break;
-                      }
-                    } else {
-                      ref.read(currentBalanceProvider.notifier).state = ref.watch(liquidBalanceInFormatProvider(ref.read(settingsProvider).btcFormat));
-                    }
-
-                    ref.read(assetExchangeProvider.notifier).state = AssetMapper.reverseMapTicker(ticker);
-                    controller.text = '';
-                    ref.read(sendTxProvider.notifier).updateAssetId(AssetMapper.reverseMapTicker(ticker));
-                    ref.read(sendTxProvider.notifier).updateAmount(0);
-                    return true;
-                  },
-                  cardBuilder: (context, index, percentThresholdX, percentThresholdY) {
-                    return Align(
-                      alignment: Alignment.center,
-                      child: cards[index],
-                    );
-                  },
-                ),
-              ),
+              buildCardSwiper(context, ref, dynamicCardHeight, cards),
             ]
                 : [
-              SizedBox(
-                height: dynamicCardHeight,
-                child: CardSwiper(
-                  scale: 0,
-                  padding: const EdgeInsets.all(0),
-                  allowedSwipeDirection: const AllowedSwipeDirection.symmetric(vertical: true),
-                  cardsCount: cards.length,
-                  onSwipe: (int previousIndex, int? currentIndex, CardSwiperDirection direction) {
-                    AssetId ticker;
-                    switch (currentIndex) {
-                      case 0:
-                        ticker = AssetId.BRL;
-                        break;
-                      case 1:
-                        ticker = AssetId.USD;
-                        break;
-                      case 2:
-                        ticker = AssetId.EUR;
-                        break;
-                      default:
-                        ticker = AssetId.BRL;
-                        break;
-                    }
-                    setState(() {
-                      ref.read(tickerProvider.notifier).state = ticker;
-                    });
-
-                    if (!sendBitcoin) {
-                      switch (ticker) {
-                        case AssetId.BRL:
-                          ref.read(currentBalanceProvider.notifier).state = fiatInDenominationFormatted(ref.watch(balanceNotifierProvider).brlBalance);
-                          break;
-                        case AssetId.USD:
-                          ref.read(currentBalanceProvider.notifier).state = fiatInDenominationFormatted(ref.watch(balanceNotifierProvider).usdBalance);
-                          break;
-                        case AssetId.EUR:
-                          ref.read(currentBalanceProvider.notifier).state = fiatInDenominationFormatted(ref.watch(balanceNotifierProvider).eurBalance);
-                          break;
-                        default:
-                          ref.read(currentBalanceProvider.notifier).state = fiatInDenominationFormatted(ref.watch(balanceNotifierProvider).brlBalance);
-                          break;
-                      }
-                    } else {
-                      ref.read(currentBalanceProvider.notifier).state = ref.watch(liquidBalanceInFormatProvider(ref.read(settingsProvider).btcFormat));
-                    }
-
-                    ref.read(assetExchangeProvider.notifier).state = AssetMapper.reverseMapTicker(ticker);
-                    controller.text = '';
-                    ref.read(sendTxProvider.notifier).updateAssetId(AssetMapper.reverseMapTicker(ticker));
-                    ref.read(sendTxProvider.notifier).updateAmount(0);
-                    return true;
-                  },
-                  cardBuilder: (context, index, percentThresholdX, percentThresholdY) {
-                    return Align(
-                      alignment: Alignment.center,
-                      child: cards[index],
-                    );
-                  },
-                ),
-              ),
+              buildCardSwiper(context, ref, dynamicCardHeight, cards),
               SizedBox(height: dynamicSizedBox),
               buildCard('Liquid', 'BTC', Colors.blueAccent, Colors.deepPurple, ref, context, true, AssetId.LBTC, titleFontSize),
             ],
           ),
-          Positioned(
-            top: titleFontSize / 1,
-            bottom: titleFontSize / 1,
-            child: GestureDetector(
-              onTap: () {
-                ref.read(sendBitcoinProvider.notifier).state = !sendBitcoin;
-                ref.read(sendTxProvider.notifier).updateAddress('');
-                ref.read(sendTxProvider.notifier).updateAmount(0);
-                ref.read(sendBlocksProvider.notifier).state = 1;
-                ref.refresh(currentBalanceProvider);
-                ref.refresh(tickerProvider);
-                ref.read(sendTxProvider.notifier).updateDrain(false);
-                controller.text = '';
-              },
-              child: CircleAvatar(
-                radius: titleFontSize,
-                backgroundColor: Colors.orange,
-                child: Icon(EvaIcons.swap, size: titleFontSize, color: Colors.black),
-              ),
-            ),
-          ),
+          buildSwapGestureDetector(context, ref, titleFontSize),
         ],
       ),
     ];
-
-
 
     if (!sendBitcoin) {
       swapCards = swapCards.reversed.toList();
@@ -257,6 +118,92 @@ class _LiquidSwapCardsState extends ConsumerState<LiquidSwapCards> {
         ],
       );
   }
+
+  Widget buildCardSwiper(BuildContext context, WidgetRef ref, double dynamicCardHeight, List<Column> cards) {
+    return SizedBox(
+      height: dynamicCardHeight,
+      child: CardSwiper(
+        scale: 0,
+        padding: const EdgeInsets.all(0),
+        allowedSwipeDirection: const AllowedSwipeDirection.symmetric(vertical: true),
+        cardsCount: cards.length,
+        onSwipe: (int previousIndex, int? currentIndex, CardSwiperDirection direction) {
+          AssetId ticker;
+          switch (currentIndex) {
+            case 0:
+              ticker = AssetId.BRL;
+              break;
+            case 1:
+              ticker = AssetId.USD;
+              break;
+            case 2:
+              ticker = AssetId.EUR;
+              break;
+            default:
+              ticker = AssetId.BRL;
+              break;
+          }
+          ref.read(tickerProvider.notifier).state = ticker;
+
+          if (!ref.read(sendBitcoinProvider)) {
+            switch (ticker) {
+              case AssetId.BRL:
+                ref.read(currentBalanceProvider.notifier).state = fiatInDenominationFormatted(ref.watch(balanceNotifierProvider).brlBalance);
+                break;
+              case AssetId.USD:
+                ref.read(currentBalanceProvider.notifier).state = fiatInDenominationFormatted(ref.watch(balanceNotifierProvider).usdBalance);
+                break;
+              case AssetId.EUR:
+                ref.read(currentBalanceProvider.notifier).state = fiatInDenominationFormatted(ref.watch(balanceNotifierProvider).eurBalance);
+                break;
+              default:
+                ref.read(currentBalanceProvider.notifier).state = fiatInDenominationFormatted(ref.watch(balanceNotifierProvider).brlBalance);
+                break;
+            }
+          } else {
+            ref.read(currentBalanceProvider.notifier).state = ref.watch(liquidBalanceInFormatProvider(ref.read(settingsProvider).btcFormat));
+          }
+
+          ref.read(assetExchangeProvider.notifier).state = AssetMapper.reverseMapTicker(ticker);
+          controller.text = '';
+          ref.read(sendTxProvider.notifier).updateAssetId(AssetMapper.reverseMapTicker(ticker));
+          ref.read(sendTxProvider.notifier).updateAmount(0);
+          return true;
+        },
+        cardBuilder: (context, index, percentThresholdX, percentThresholdY) {
+          return Align(
+            alignment: Alignment.center,
+            child: cards[index],
+          );
+        },
+      ),
+    );
+  }
+
+  Widget buildSwapGestureDetector(BuildContext context, WidgetRef ref, double titleFontSize) {
+    return Positioned(
+      top: titleFontSize / 1,
+      bottom: titleFontSize / 1,
+      child: GestureDetector(
+        onTap: () {
+          ref.read(sendBitcoinProvider.notifier).state = !ref.read(sendBitcoinProvider);
+          ref.read(sendTxProvider.notifier).updateAddress('');
+          ref.read(sendTxProvider.notifier).updateAmount(0);
+          ref.read(sendBlocksProvider.notifier).state = 1;
+          ref.refresh(currentBalanceProvider);
+          ref.refresh(tickerProvider);
+          ref.read(sendTxProvider.notifier).updateDrain(false);
+          controller.text = '';
+        },
+        child: CircleAvatar(
+          radius: titleFontSize,
+          backgroundColor: Colors.orange,
+          child: Icon(EvaIcons.swap, size: titleFontSize, color: Colors.black),
+        ),
+      ),
+    );
+  }
+
 
   Widget _buildMaxButton(WidgetRef ref, double dynamicPadding, double dynamicFontSize, String btcFormat, titleFontSize) {
     final sendBitcoin = ref.watch(sendBitcoinProvider);
@@ -362,7 +309,7 @@ class _LiquidSwapCardsState extends ConsumerState<LiquidSwapCards> {
                           return Text('${btcInDenominationFormatted(valueToReceive.toDouble(), btcFormat, !sendBitcoin)}', style: TextStyle(color: Colors.white, fontSize: titleFontSize), textAlign: TextAlign.center);
                         }
                       },
-                      loading: () => controller.text.isEmpty ?Text("0", style: TextStyle(color: Colors.white, fontSize: titleFontSize), textAlign: TextAlign.center) : Text('Loading...', style: TextStyle(color: Colors.white, fontSize: titleFontSize), textAlign: TextAlign.center),
+                      loading: () => controller.text.isEmpty ?Text("0", style: TextStyle(color: Colors.white, fontSize: titleFontSize), textAlign: TextAlign.center) : Center(child: LoadingAnimationWidget.prograssiveDots(size: titleFontSize, color: Colors.white)),
                       error: (error, stack) => Text('Error: $error', textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontSize: titleFontSize)),
                     );
                   },
