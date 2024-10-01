21078404d29431bae7a7f30ea42e7ff488d38de0
joaolobo <joao.lobom@gmail.com>
2024-10-01 10:04:49 +0100

    Change default to blockstream

--- lib/providers/settings_provider.dart
+++ lib/providers/settings_provider.dart
@@ -10,9 +10,9 @@ final initialSettingsProvider = FutureProvider<Settings>((ref) async {
       final language = box.get('language', defaultValue: languageIsPortuguese ? 'pt' : 'en');
       final btcFormat = box.get('btcFormat', defaultValue: 'BTC');
       final backup = box.get('backup', defaultValue: false);
-      final bitcoinElectrumNode = box.get('bitcoinElectrumNode', defaultValue: 'wes.bullbitcoin.com:50002');
-      final liquidElectrumNode = box.get('liquidElectrumNode', defaultValue: 'les.bullbitcoin.com:995');
-      final nodeType = box.get('nodeType', defaultValue: 'Bull Bitcoin');
+      final bitcoinElectrumNode = box.get('bitcoinElectrumNode', defaultValue: 'blockstream.info:700');
+      final liquidElectrumNode = box.get('liquidElectrumNode', defaultValue: 'blockstream.info:995');
+      final nodeType = box.get('nodeType', defaultValue: 'Blockstream');
 
       return Settings(currency: currency, language: language, btcFormat: btcFormat, online: true, backup: backup, bitcoinElectrumNode: bitcoinElectrumNode, liquidElectrumNode: liquidElectrumNode, nodeType: nodeType);
 });
