82ec0d8ada740d258b1623b1c289167960f5d9a5
Saleciani <47111823+Saleciani@users.noreply.github.com>
2025-08-07 10:46:50 +0100

    Improve bundle size

--- android/app/src/main/kotlin/com/example/satsails/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/satsails/MainActivity.kt
@@ -1,5 +1,14 @@
 package com.satsails.Satsails
 
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterFragmentActivity
+import android.os.Bundle
+
 class MainActivity: FlutterFragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // This line enables edge-to-edge display.
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        super.onCreate(savedInstanceState)
+    }
 }
